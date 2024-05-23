fbb3d3a6dafc4bc94c1ab96c989f674ef1059631
fall2023 <fall2023@SELAB8-016>
2024-05-23 12:37:14 +0500

    Add project files.

---
+++ Models/ProductContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication20.Models
+{
+    public class ProductContext: DbContext
+    {
+        public ProductContext(DbContextOptions<ProductContext> options) : base(options) { }
+
+        public DbSet<Products> Products { get; set; } = null!;
+    }
+}
---
+++ Models/Products.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication20.Models
+{
+    public class Products
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
